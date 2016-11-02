d679943a7f1467bb71c2b325869b42e93a4be60d
Mecit Semerci <mecit.semerci@gmail.com>
2016-11-02 17:56:09 +0300

    Charts added to dashboard

--- Jeton.Admin.Web/Controllers/HomeController.cs
+++ Jeton.Admin.Web/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using AutoMapper;
+﻿using System;
+using System.Collections.Generic;
+using AutoMapper;
 using Jeton.Admin.Web.Models;
 using Jeton.Core.Entities;
 using Jeton.Services.AppService;
@@ -14,7 +16,10 @@ namespace Jeton.Admin.Web.Controllers
     //[Authorize]
     public class HomeController : Controller
     {
-        private readonly MapperConfiguration _config = new MapperConfiguration(cfg => cfg.CreateMap<Setting, SettingModel>());
+        private readonly MapperConfiguration _settingMapperConfiguration = new MapperConfiguration(cfg => cfg.CreateMap<Setting, SettingModel>());
+        private readonly MapperConfiguration _tokenMapperConfiguration = new MapperConfiguration(cfg => cfg.CreateMap<Token, TokenModel>());
+        private readonly MapperConfiguration _appMapperConfiguration = new MapperConfiguration(cfg => cfg.CreateMap<App, AppModel>());
+
         private readonly IAppService _appService;
         private readonly ITokenService _tokenService;
         private readonly IUserService _userService;
@@ -31,17 +36,97 @@ namespace Jeton.Admin.Web.Controllers
         public ActionResult Index()
         {
 
-            ViewBag.AppCount = _appService.GetApps().Count(a => !a.IsDeleted.HasValue || (a.IsDeleted == false));
-            ViewBag.TokenCount = _tokenService.GetActiveTokensCount();
-            ViewBag.UserCount = _userService.GetUsers().Count();
+            #region Top Bar Count Items        
 
+            BindTopBarCounts();
 
-            //Essential Settings
-            var mapper = _config.CreateMapper();
-            var essentialSettings = _settingService.GetAllSettings().AsEnumerable().Where(s => s.IsEssential).Select(s => mapper.Map<SettingModel>(s)).ToList();
-            ViewBag.Settings = essentialSettings;
+            #endregion
+
+            #region Essential Settings
+
+            BindSettings();
+
+            #endregion
+
             return View();
         }
+
+        public JsonResult GetActiveApps()
+        {
+            IEnumerable<AppModel> apps;
+            try
+            {
+                var appMapper = _appMapperConfiguration.CreateMapper();
+                apps = _appService.GetApps().AsEnumerable().Where(a => !a.IsDeleted.HasValue || (a.IsDeleted.Value == false)).Select(t => appMapper.Map<AppModel>(t)).ToList();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(apps, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult GetTokens()
+        {
+            IEnumerable<TokenModel> tokens;
+            try
+            {
+                var tokenMapper = _tokenMapperConfiguration.CreateMapper();
+                tokens = _tokenService.GetTokens().AsEnumerable().Select(t => tokenMapper.Map<TokenModel>(t)).ToList();
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return Json(tokens, JsonRequestBehavior.AllowGet);
+
+        }
+
+        private void BindTopBarCounts()
+        {
+            try
+            {
+                //App Count
+                ViewBag.AppCount = _appService.GetApps().Count(a => !a.IsDeleted.HasValue || (a.IsDeleted == false));
+
+                //Active Token Count
+                ViewBag.TokenCount = _tokenService.GetActiveTokensCount();
+
+                //User Count
+                ViewBag.UserCount = _userService.GetUsers().Count();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        private void BindSettings()
+        {
+            try
+            {
+                var settingMapper = _settingMapperConfiguration.CreateMapper();
+                var essentialSettings = _settingService.GetAllSettings()
+                                                        .AsEnumerable()
+                                                        .Where(s => s.IsEssential)
+                                                        .Select(s => settingMapper.Map<SettingModel>(s))
+                                                        .ToList();
+                ViewBag.Settings = essentialSettings;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
 
     }
 }
